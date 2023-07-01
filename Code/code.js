e9123896e5c48d074491f4fa5c9d592d8bc29e89
x37verheij <x37verheij@gmail.com>
2023-07-01 17:51:25 +0200

    Knop toegevoegd.

--- Code/code.js
+++ Code/code.js
@@ -1,5 +1,3 @@
-// NB: Deze code is Copy-pasted uit de editor. De libraries zijn Custom voor de Microbit Editor
-// Run de code niet hier, download het naar de Microbit, en test het zelf 
 basic.showLeds(`
     . . . . .
     . # # # .
@@ -11,6 +9,18 @@ makerbit.connectSerialMp3(DigitalPin.P13, DigitalPin.P14)
 makerbit.setMp3Volume(30)
 basic.forever(function () {
     if (input.acceleration(Dimension.Y) > 200) {
+        basic.showIcon(IconNames.Square)
         makerbit.playMp3Track(randint(1, 3), 1)
+    }
+    if (input.pinIsPressed(TouchPin.P0)) {
+    	
+    } else {
+        basic.showIcon(IconNames.Heart)
+        makerbit.playMp3Track(randint(4, 19), 1)
+    }
+    if (input.buttonIsPressed(Button.B)) {
+        // Paardengeluid tracks 1-3 en Quotes tracks 4-19
+        basic.showIcon(IconNames.EighthNote)
+        makerbit.playMp3Track(randint(4, 19), 1)
     }
 })
